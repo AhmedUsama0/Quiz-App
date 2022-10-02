07435ed452fda16fcb369166ea0797c7d7b96772
Ahmed Usama <aomn040@gmail.com>
2022-10-02 17:02:08 +0200

    update 2

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 "use strict";
 let score = 0,
-  userAnswers;
-const questions = Array.from(document.querySelectorAll(".question"));
-const scoreMessage = document.querySelector("#score");
-const leftQuestions  = document.querySelector("#left_Questions");
-leftQuestions.textContent = `${questions.length} Questions Left`;
-let answers = ["inline", "href", "3ways", "c"];
-let questionIndex = Math.round(Math.random() * (questions.length - 1));
-questions[questionIndex].style.display = "block";
+  userAnswers,
+  questionIndex;
 
-document.querySelector("#next").addEventListener("click", next);
+const questions = Array.from(document.querySelectorAll(".question"));
+
+const scoreMessage = document.querySelector("#score");
+
+const leftQuestions = document.querySelector("#left_Questions");
+
+let answers = ["inline", "href", "3ways", "c"];
+
+startGame();
+
+const nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", next);
 
 function next() {
   checkAnswer();
   if (userAnswers.length === questions.length) {
-    document.querySelector("#next").removeEventListener("click", next);
-    scoreMessage.innerHTML = `<p>Your Score is ${score}/${questions.length}</p>`;
-    scoreMessage.style.opacity = "0.8";
-    scoreMessage.style.zIndex = "30";
+    nextButton.removeEventListener("click", next);
+    scoreMessage.innerHTML = `<p>Your Score is ${score}/${questions.length}</p> <button id="play_again">Play Again</button>`;
+    scoreMessage.style.opacity = "0.8", scoreMessage.style.zIndex = "30";
+    document.querySelector("#play_again").addEventListener("click", playAgain);
   }
 }
 function nextQuestion() {
-  questions.forEach((question) => {
-    question.style.display = "none";
-  });
-  if (questionIndex < questions.length - 1) questionIndex++;
-  else questionIndex = 0;
-
+  questions.forEach(question => question.style.display = "none");
+  questionIndex < questions.length - 1 ? questionIndex++ : questionIndex = 0;
   questions[questionIndex].style.display = "block";
 }
 function checkAnswer() {
@@ -39,7 +40,21 @@ function checkAnswer() {
       }
     });
   });
-  
+
   leftQuestions.textContent = `${questions.length - userAnswers.length} Questinos Left`;
   nextQuestion();
+}
+
+function startGame() {
+  questionIndex = Math.round(Math.random() * (questions.length - 1));
+  questions[questionIndex].style.display = "block";
+  leftQuestions.textContent = `${questions.length} Questions Left`;
+}
+function playAgain() {
+  score = 0;
+  userAnswers.forEach((checkbox) => (checkbox.checked = false));
+  scoreMessage.style.opacity = "0", scoreMessage.style.zIndex = "-30";
+  nextButton.addEventListener("click", next);
+  questions.forEach(question => question.style.display = "none");
+  startGame();
 }
